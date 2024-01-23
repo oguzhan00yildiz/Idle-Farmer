e53db2aa28fa98d0697dbdcd17c05e6399a2fd68
Oguzhan Yildiz <oguzhan00yildiz@gmail.com>
2024-01-23 14:26:56 +0200

    Parrelsync added

--- Assets/Scripts/Multiplayer/Connection.cs
+++ Assets/Scripts/Multiplayer/Connection.cs
@@ -5,29 +5,45 @@ using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.UI;
 
-public class Connection : MonoBehaviour
+public class Connection : NetworkBehaviour
 {
     [SerializeField] private Button hostButton;
     [SerializeField] private Button joinButton;
+    [SerializeField] private string playerName;
     void Start()
     {
         hostButton.onClick.AddListener(Host);
         joinButton.onClick.AddListener(Join);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     
     }
 
     void Host()
     {
         NetworkManager.Singleton.StartHost();
+        
     }
 
     void Join()
     {
         NetworkManager.Singleton.StartClient();
+        
+    }
+
+    [ClientRpc]
+    void RpcClientConnected()
+    {
+        RpcServerConnected();
+    }
+
+    [ServerRpc]
+    void RpcServerConnected()
+    {
+        Debug.Log(playerName + " has connected to the server");
     }
 }
