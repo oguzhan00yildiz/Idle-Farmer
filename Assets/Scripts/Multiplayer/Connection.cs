740b51951657a13ab9f32bb5db81a6c43c612ebe
Oguzhan Yildiz <oguzhan00yildiz@gmail.com>
2024-01-23 15:13:18 +0200

    Client Id and Player names are showing
    Client Id and Player name is showing through Log.

--- Assets/Scripts/Multiplayer/Connection.cs
+++ Assets/Scripts/Multiplayer/Connection.cs
@@ -14,10 +14,11 @@ public class Connection : NetworkBehaviour
     {
         hostButton.onClick.AddListener(Host);
         joinButton.onClick.AddListener(Join);
-        
+
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
     }
 
-    // Update is called once per frame
     void Update()
     {
      
@@ -35,15 +36,40 @@ public class Connection : NetworkBehaviour
         
     }
 
-    [ClientRpc]
-    void RpcClientConnected()
+    void OnClientConnected(ulong clientId)
     {
-        RpcServerConnected();
+        // If we are server, say who connected and then stop
+        if (IsHost)
+        {
+            Debug.Log(clientId + " connected");
+            return;
+        }
+
+        // Client calls this
+        ReceiveNameServerRpc(clientId, playerName);
     }
 
-    [ServerRpc]
-    void RpcServerConnected()
+    void OnClientDisconnected(ulong clientId)
     {
-        Debug.Log(playerName + " has connected to the server");
+        Debug.Log(clientId + " disconnected");
     }
+
+    
+    [ServerRpc(RequireOwnership = false)]
+    void ReceiveNameServerRpc(ulong clientId, string name)
+    {
+        Debug.Log("Received " + name + " from " + clientId);
+
+        // Now we are server and we call this
+        // and it will run on all clients
+        TellMeHisNameClientRpc(clientId, name);
+    }
+
+    // Runs on client(s) but is called by server
+    [ClientRpc]
+    void TellMeHisNameClientRpc(ulong clientId, string name)
+    {
+        Debug.Log("Client " + clientId + "'s name is " + name);
+    }
+
 }
