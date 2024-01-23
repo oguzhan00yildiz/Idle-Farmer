bb05dee7a0d5294ab548c14924d1f85693f5860c
Oguzhan Yildiz <oguzhan00yildiz@gmail.com>
2024-01-23 15:30:47 +0200

    added Scene switch

--- Assets/Scripts/Multiplayer/Connection.cs
+++ Assets/Scripts/Multiplayer/Connection.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using Unity.Netcode;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Connection : NetworkBehaviour
 {
     [SerializeField] private Button hostButton;
     [SerializeField] private Button joinButton;
+    [SerializeField] private Button startButton;
+    [SerializeField] private GameObject startPanel;
+    [SerializeField] private GameObject connectionPanel;
     [SerializeField] private string playerName;
     void Start()
     {
         hostButton.onClick.AddListener(Host);
         joinButton.onClick.AddListener(Join);
+        startButton.onClick.AddListener(StartGame);
 
         NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
         NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
@@ -27,13 +31,24 @@ public class Connection : NetworkBehaviour
     void Host()
     {
         NetworkManager.Singleton.StartHost();
-        
+        connectionPanel.SetActive(false);
+        startPanel.SetActive(true);
     }
 
     void Join()
     {
         NetworkManager.Singleton.StartClient();
-        
+        connectionPanel.SetActive(false);
+        startPanel.SetActive(true);
+    }
+
+    void StartGame()
+    {
+        if (NetworkManager.Singleton.IsHost)
+        {
+            NetworkManager.Singleton.SceneManager.LoadScene("SampleScene",LoadSceneMode.Single);
+        }
+          
     }
 
     void OnClientConnected(ulong clientId)
